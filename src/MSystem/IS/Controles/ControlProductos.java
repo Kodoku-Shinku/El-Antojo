2b99ab871d5e787926a44515b23902b83b0a495b
BettyAR <arbett.hinaharu.94@gmail.com>
2018-03-12 13:12:04 -0600

    Creacion de vistaConsultarFaltantes, y EntidadProductof

---
+++ src/MSystem/IS/Controles/ControlProductos.java
@@ -0,0 +1,11 @@
+package MSystem.IS.Controles;
+
+import MSystem.IS.Servicios.ServicioProductos;
+
+public class ControlProductos {
+
+	public ControlProductos(ServicioProductos servProduct) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
