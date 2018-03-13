d4d0ec41b5a6383e0e8b45e3291b5d56ed8ce9b6
Marisolch <marisolchj@gmail.com>
2018-03-13 16:14:14 -0600

    vistas de productos, admin,actualizar productos

---
+++ src/MSystem/IS/Vistas/VistaAdministrador.java
@@ -0,0 +1,54 @@
+package MSystem.IS.Vistas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLayeredPane;
+import javax.swing.JButton;
+
+public class VistaAdministrador extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VistaAdministrador frame = new VistaAdministrador();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VistaAdministrador() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		contentPane.add(layeredPane, BorderLayout.CENTER);
+		
+		JButton btnPersonal = new JButton("Personal");
+		btnPersonal.setBounds(165, 35, 89, 23);
+		layeredPane.add(btnPersonal);
+		
+		JButton btnProductos = new JButton("Productos");
+		btnProductos.setBounds(165, 136, 89, 23);
+		layeredPane.add(btnProductos);
+	}
+}
---
+++ src/MSystem/IS/Vistas/VistaProductos.java
@@ -0,0 +1,65 @@
+package MSystem.IS.Vistas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLayeredPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VistaProductos extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VistaProductos frame = new VistaProductos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VistaProductos() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		contentPane.add(layeredPane, BorderLayout.CENTER);
+		
+		JButton btnNotificarFaltantes = new JButton("Consultar Faltantes");
+		btnNotificarFaltantes.setBounds(125, 25, 148, 23);
+		layeredPane.add(btnNotificarFaltantes);
+		
+		JButton btnNewButton = new JButton("Generar Lista");
+		btnNewButton.setBounds(125, 98, 148, 23);
+		layeredPane.add(btnNewButton);
+		
+		JButton btnActualizarProductos = new JButton("Actualizar Productos");
+		btnActualizarProductos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnActualizarProductos.setBounds(125, 165, 148, 23);
+		layeredPane.add(btnActualizarProductos);
+	}
+
+}
