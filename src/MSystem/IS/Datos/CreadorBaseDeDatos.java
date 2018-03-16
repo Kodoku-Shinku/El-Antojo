a95eb61ff86c91c004c7c7853da02e354ff4c125
Marisolch <marisolchj@gmail.com>
2018-03-15 19:14:10 -0600

    Actualizarcreador de Base de datos

--- src/MSystem/IS/Datos/CreadorBaseDeDatos.java
+++ src/MSystem/IS/Datos/CreadorBaseDeDatos.java
@@ -18,23 +18,26 @@ public class CreadorBaseDeDatos {
 			Connection connection = ManejadorBD.dameConnection();
 
 			Statement statement = connection.createStatement();
-			statement.execute("create table Platillos(platilloId INTEGER PRIMARY KEY not null GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), nombrePlatillo varchar(40), tipo varchar(10), precio double precision)"); // Aqu� el identificador es para diferenciar entre bebidas y platillos
+			//statement.execute("create table Platillos(platilloId INTEGER PRIMARY KEY not null GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), nombrePlatillo varchar(40), tipo varchar(10), precio double precision)"); // Aqu� el identificador es para diferenciar entre bebidas y platillos
 			//statement.execute("DROP TABLE Platillos");
-			System.out.println("Tabla de Platillos creada exitosamente");
-			
-			statement.execute("create table Pedidos(IDPlatillo int, cantidadPlatillo int, noMesa int)"); // , CONSTRAINT primary_key PRIMARY KEY (autorId)
-			//statement.execute("DROP TABLE Pedidos");
-			System.out.println("Tabla de Pedidos creada exitosamente");
-			
-			statement.execute("create table PedidosF(IDPlatillo int, cantidadPlatillo int, noMesa int)");
-			System.out.println("Tabla de PedidosF creada exitosamente");
-
-			statement.execute("create table ProductosCocina(Nombre varchar(20), cantidad double)");
-			System.out.println("Tabla de ProductosCocina creada exitosamente");
+//			System.out.println("Tabla de Platillos creada exitosamente");
 //			
-			statement.execute("create table ProductosF(Nombre varchar(20), cantidad double)");
-			//statement.execute("DROP TABLE ProductosFaltantes");
-			System.out.println("Tabla de ProductosF creada exitosamente");
+//			statement.execute("create table Pedidos(IDPlatillo int, cantidadPlatillo int, noMesa int)"); // , CONSTRAINT primary_key PRIMARY KEY (autorId)
+//			//statement.execute("DROP TABLE Pedidos");
+//			System.out.println("Tabla de Pedidos creada exitosamente");
+//			
+//			statement.execute("create table PedidosF(IDPlatillo int, cantidadPlatillo int, noMesa int)");
+//			System.out.println("Tabla de PedidosF creada exitosamente");
+//
+//			statement.execute("create table ProductosCocina(Nombre varchar(20), cantidad double)");
+//			System.out.println("Tabla de ProductosCocina creada exitosamente");
+////			
+//			statement.execute("create table ProductosF(Nombre varchar(20), cantidad double)");
+//			//statement.execute("DROP TABLE ProductosFaltantes");
+//			System.out.println("Tabla de ProductosF creada exitosamente");
+			
+			statement.execute("create table ProductosAdmin(nombre varchar(20), cantidad double)");
+			System.out.println("Tabla de ProductosAdmin creada exitosamente");
 			
 			ManejadorBD.termina();
 		}
