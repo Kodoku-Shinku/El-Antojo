53b83015b31ad4bb3a4031de861eb1cd38fd9422
RosaToral <rosa28gato@gmail.com>
2018-03-19 11:48:01 -0600

    No se cierra en automatico

--- src/MSystem/IS/Vistas/VistaNotificarFaltantes.java
+++ src/MSystem/IS/Vistas/VistaNotificarFaltantes.java
@@ -43,7 +43,7 @@ public class VistaNotificarFaltantes extends JFrame {
 		if (lista == null)
 			JOptionPane.showMessageDialog(null, "No se puede cargar la base de datos");
 		else {
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			setBounds(100, 100, 450, 300);
 			contentPane = new JPanel();
 			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
