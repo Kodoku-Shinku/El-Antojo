bdbdf1ad65d01e63f84d3144f209419546c96052
RosaToral <rosa28gato@gmail.com>
2018-03-29 18:05:11 -0600

    Enlace entre las vistas VistaAdministrador y VistaActualizarEmpleado

--- src/MSystem/IS/Vistas/VistaActualizarEmpleado.java
+++ src/MSystem/IS/Vistas/VistaActualizarEmpleado.java
@@ -44,6 +44,7 @@ public class VistaActualizarEmpleado extends JFrame implements ItemListener {
 	private JTextArea textArea;
 	private JLabel enEmp;
 	private JComboBox<String> comboBox;
+	private ArrayList<Empleado> emp;
 	private Empleado empl;
 	private String campo = "";
 	private String name = "";
@@ -53,11 +54,12 @@ public class VistaActualizarEmpleado extends JFrame implements ItemListener {
 	 * Create the frame.
 	 */
 	public VistaActualizarEmpleado(ControlAdministracion controlAdmin) {
-		
-		empl = new Empleado("","","","","","");
-		ArrayList<Empleado> emp = controlAdmin.cargarLista();
+
+		empl = new Empleado("", "", "", "", "", "");
+		emp = controlAdmin.cargarLista();
 		setTitle("Actualizar Empleado");
-		setIconImage(Toolkit.getDefaultToolkit().getImage(VistaActualizarEmpleado.class.getResource("/MSystem/IS/Vistas/el_antojo.png")));
+		setIconImage(Toolkit.getDefaultToolkit()
+				.getImage(VistaActualizarEmpleado.class.getResource("/MSystem/IS/Vistas/el_antojo.png")));
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 505, 523);
 		setResizable(false);
@@ -66,79 +68,54 @@ public class VistaActualizarEmpleado extends JFrame implements ItemListener {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		nombre = new JTextField();
 		nombre.setName("nombre");
 		nombre.setEditable(false);
 		nombre.setBounds(249, 169, 195, 20);
 		contentPane.add(nombre);
 		nombre.setColumns(10);
-		
+
 		apPat = new JTextField();
 		apPat.setName("apat");
 		apPat.setEditable(false);
 		apPat.setBounds(249, 212, 195, 20);
 		contentPane.add(apPat);
 		apPat.setColumns(10);
-		
+
 		apMat = new JTextField();
 		apMat.setName("amat");
 		apMat.setEditable(false);
 		apMat.setBounds(249, 256, 195, 20);
 		contentPane.add(apMat);
 		apMat.setColumns(10);
-		
+
 		telefono = new JTextField();
 		telefono.setName("tel");
 		telefono.setEditable(false);
 		telefono.setBounds(249, 387, 195, 20);
 		contentPane.add(telefono);
 		telefono.setColumns(10);
-		
+
 		textArea = new JTextArea();
 		textArea.setName("domicilio");
 		textArea.setEditable(false);
 		textArea.setBounds(249, 298, 195, 65);
 		contentPane.add(textArea);
-		
+
 		JLabel lblNumeroDeEmpleado = new JLabel("Numero de Empleado");
 		lblNumeroDeEmpleado.setBounds(66, 138, 134, 14);
 		contentPane.add(lblNumeroDeEmpleado);
-		
+
 		enEmp = new JLabel("");
 		enEmp.setBounds(249, 138, 46, 14);
 		contentPane.add(enEmp);
-		
+
 		comboBox = new JComboBox<String>();
-		comboBox.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent arg0) {
-				int index = comboBox.getSelectedIndex();
-				empl = emp.get(index);
-				nombre.setText(empl.getNombre());
-				apPat.setText(empl.getApellidoPaterno());
-				apMat.setText(empl.getApellidoMaterno());
-				telefono.setText(empl.getTelefono());
-				textArea.setText(empl.getDireccion());
-				enEmp.setText(empl.getid()+"");
-			}
-		});
-//		comboBox.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				if(e.getSource() == comboBox){
-//					int index = comboBox.getSelectedIndex();
-//					empl = emp.get(index);
-//					nombre.setText(empl.getNombre());
-//					apPat.setText(empl.getApellidoPaterno());
-//					apMat.setText(empl.getApellidoMaterno());
-//					telefono.setText(empl.getTelefono());
-//					textArea.setText(empl.getDireccion());
-//					enEmp.setText(empl.getid()+"");
-//				}
-//			}
-//		});
+		comboBox.addItemListener(this);
 		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] {}
-		
-		){
+
+		) {
 			/**
 			 * 
 			 */
@@ -149,46 +126,47 @@ public class VistaActualizarEmpleado extends JFrame implements ItemListener {
 				return emp.size();
 			}
 		});
-		//comboBox.addItem("");
-		for(int i = 0; i < emp.size(); i++){
-			comboBox.addItem(emp.get(i).getid() + " - " + emp.get(i).getNombre() + " " + emp.get(i).getApellidoPaterno() + " " + emp.get(i).getApellidoMaterno());
+		// comboBox.addItem("");
+		for (int i = 0; i < emp.size(); i++) {
+			comboBox.addItem(emp.get(i).getid() + " - " + emp.get(i).getNombre() + " " + emp.get(i).getApellidoPaterno()
+					+ " " + emp.get(i).getApellidoMaterno());
 		}
 		comboBox.setBounds(66, 60, 284, 20);
 		contentPane.add(comboBox);
-		
+
 		JLabel lblEmpleado = new JLabel("Empleado");
 		lblEmpleado.setBounds(32, 22, 78, 14);
 		contentPane.add(lblEmpleado);
-		
+
 		JLabel lblNewLabel = new JLabel("Datos");
 		lblNewLabel.setBounds(32, 115, 46, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		chxDomicilio = new JCheckBox("Domicilio");
 		chxDomicilio.setBounds(66, 299, 97, 23);
 		chxDomicilio.addItemListener(this);
 		contentPane.add(chxDomicilio);
-		
+
 		chxTelefono = new JCheckBox("Telefono");
 		chxTelefono.setBounds(66, 386, 97, 23);
 		chxTelefono.addItemListener(this);
 		contentPane.add(chxTelefono);
-		
+
 		chxNombre = new JCheckBox("Nombre");
 		chxNombre.setBounds(66, 168, 97, 23);
 		chxNombre.addItemListener(this);
 		contentPane.add(chxNombre);
-		
+
 		chxApellidoMaterno = new JCheckBox("Apellido Materno");
 		chxApellidoMaterno.setBounds(66, 255, 134, 23);
 		chxApellidoMaterno.addItemListener(this);
 		contentPane.add(chxApellidoMaterno);
-		
+
 		chxApellidoPaterno = new JCheckBox("Apellido Paterno");
 		chxApellidoPaterno.setBounds(66, 211, 134, 23);
 		chxApellidoPaterno.addItemListener(this);
 		contentPane.add(chxApellidoPaterno);
-		
+
 		JButton btnCancelar = new JButton("Cancelar");
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -197,35 +175,36 @@ public class VistaActualizarEmpleado extends JFrame implements ItemListener {
 		});
 		btnCancelar.setBounds(228, 448, 89, 23);
 		contentPane.add(btnCancelar);
-		
+
 		JButton btnAceptar = new JButton("Aceptar");
 		btnAceptar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-//				
-				if(chxNombre.isSelected() == true)
+				if (chxNombre.isSelected() == true)
 					nValor = nombre.getText();
-				if(chxApellidoMaterno.isSelected() == true)
+				if (chxApellidoMaterno.isSelected() == true)
 					nValor = apMat.getText();
-				if(chxApellidoPaterno.isSelected() == true)
+				if (chxApellidoPaterno.isSelected() == true)
 					nValor = apPat.getText();
-				if(chxDomicilio.isSelected() == true)
+				if (chxDomicilio.isSelected() == true)
 					nValor = textArea.getText();
-				if(chxTelefono.isSelected() == true)
+				if (chxTelefono.isSelected() == true)
 					nValor = telefono.getText();
-				//if(campo.equals("") || name.equals("") || nValor.equals(""))
-					//JOptionPane.showMessageDialog(null, "No ha seleccionado un campo");
-				//else{
-					if(controlAdmin.actualiza(empl, campo, nValor)){
-						JOptionPane.showMessageDialog(null, "Dato " + name + " catualizado correctamente");
-						repaint();
+				if (campo.equals("") || name.equals("") || nValor.equals("")) {
+					int option = JOptionPane.showConfirmDialog(null,
+							"No ha seleccionado campos\n " + "¿Desea continar?");
+					if (JOptionPane.NO_OPTION == option) {
+						dispose();
 					}
-					else{
-						String error = "Campo "+ name +" excedido o no ha ingresado valores";
-						if(campo.equals("tel"))
+				} else {
+					if (controlAdmin.actualiza(empl, campo, nValor)) {
+						JOptionPane.showMessageDialog(null, "Dato " + name + " catualizado correctamente");
+					} else {
+						String error = "Campo " + name + " excedido o no ha ingresado valores";
+						if (campo.equals("tel"))
 							error = "Numero de telefono invalido o muy largo";
 						JOptionPane.showMessageDialog(null, "Error al actualizar el campo:\n " + error);
 					}
-				//}
+				}
 			}
 		});
 		btnAceptar.setBounds(355, 448, 89, 23);
@@ -235,60 +214,66 @@ public class VistaActualizarEmpleado extends JFrame implements ItemListener {
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		// TODO Auto-generated method stub
-		if(chxNombre.isSelected() == true){
-			nombre.setEditable(true);
-			campo = nombre.getName();
-			name = "Nombre";
+		if (e.getSource() == comboBox) {
+			int index = comboBox.getSelectedIndex();
+			empl = emp.get(index);
+			nombre.setText(empl.getNombre());
+			apPat.setText(empl.getApellidoPaterno());
+			apMat.setText(empl.getApellidoMaterno());
+			telefono.setText(empl.getTelefono());
+			textArea.setText(empl.getDireccion());
+			enEmp.setText(empl.getid() + "");
+		} else {
+			if (chxNombre.isSelected() == true) {
+				nombre.setEditable(true);
+				campo = nombre.getName();
+				name = "Nombre";
+			} else {
+				nombre.setEditable(false);
+				// campo = "";
+				// nombre.setText("");
+			}
+
+			if (chxApellidoMaterno.isSelected() == true) {
+				apMat.setEditable(true);
+				campo = apMat.getName();
+				name = "Apellido Materno";
+			} else {
+				apMat.setEditable(false);
+				// campo = "";
+				// apMat.setText("");
+			}
+
+			if (chxApellidoPaterno.isSelected() == true) {
+				apPat.setEditable(true);
+				campo = apPat.getName();
+				name = "Apellido Paterno";
+			} else {
+				apPat.setEditable(false);
+				// campo = "";
+				// apPat.setText("");
+			}
+
+			if (chxDomicilio.isSelected() == true) {
+				textArea.setEditable(true);
+				campo = textArea.getName();
+				name = "Domicilio";
+			} else {
+				textArea.setEditable(false);
+				// campo = "";
+				// textArea.setText("");
+			}
+
+			if (chxTelefono.isSelected() == true) {
+				telefono.setEditable(true);
+				campo = telefono.getName();
+				name = "Telefono";
+			} else {
+				telefono.setEditable(false);
+				// campo = "";
+				// telefono.setText("");
+			}
 		}
-		else{
-			nombre.setEditable(false);
-			//campo = "";
-			//nombre.setText("");
-		}
-		
-		if(chxApellidoMaterno.isSelected() == true){
-			apMat.setEditable(true);
-			campo = apMat.getName();
-			name = "Apellido Materno";
-		}
-		else{
-			apMat.setEditable(false);
-			//campo = "";
-			//apMat.setText("");
-		}
-		
-		if(chxApellidoPaterno.isSelected() == true){
-			apPat.setEditable(true);
-			campo = apPat.getName();
-			name = "Apellido Paterno";
-		}
-		else{
-			apPat.setEditable(false);
-			//campo = "";
-			//apPat.setText("");
-		}
-		
-		if(chxDomicilio.isSelected() == true){
-			textArea.setEditable(true);
-			campo = textArea.getName();
-			name = "Domicilio";
-		}
-		else{
-			textArea.setEditable(false);
-			//campo = "";
-			//textArea.setText("");
-		}
-		
-		if(chxTelefono.isSelected() == true){
-			telefono.setEditable(true);
-			campo = telefono.getName();
-			name = "Telefono";
-		}
-		else{
-			telefono.setEditable(false);
-			//campo = "";
-		//	telefono.setText("");
-		}
-		
+
 	}
 }
